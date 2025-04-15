5a02269a7e3497afac58d7a92381d3a2032db94d
J-P-Crow <jaredpcrowther@gmail.com>
2025-04-15 15:07:16 -0600

    Project 7 and index update

---
+++ Projects/Project7/myscripts.js
@@ -0,0 +1,57 @@
+// JavaScript Document
+
+PrintButton1 = new Image
+	PrintButton2 = new Image
+	logo1 = new Image
+	logo2 = new Image
+if (document.images) {
+	PrintButton1.src = "images/print5.gif"
+	PrintButton2.src = "images/print6.gif"
+	logo1.src = "images/logo1.png"
+	logo2.src = "images/logo2.png"
+}
+
+
+		var imgArray = new Array(
+			'Chef2Lg.jpg',
+			'Food1Lg.jpg',
+			'ForkKnifeLg.jpg',
+			'Chef4Lg.jpg'
+			
+
+		);
+
+
+		var titleArray = new Array(
+			'Master Chef Pierre DeBois',
+			'Food Basket',
+			'Fork and Knife on a plate with a napkin',
+			'Master Chef Pierre DeBois'
+			
+		);
+			
+		var imgPath = "./Images/CookingFS/";
+		
+		function swapImage(imgID) {
+
+			var theImage = document.getElementById('theImage');
+			var textDiv = document.getElementById('bottomText');
+
+			var newImg;
+			var textTitle;
+
+			newImg = imgArray[imgID];
+			theImage.src = imgPath + newImg;
+
+			textTitle=titleArray[imgID];
+
+      			textDiv.innerHTML = textTitle;
+		}
+			
+		function preloadImages() {		
+			for(var i = 0; i < imgArray.length; i++) {
+				var tmpImg = new Image;
+				tmpImg.src = imgPath + imgArray[i];
+			}
+		}
+		
